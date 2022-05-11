bd4c438cfecb239ffafce91969d524cb53897201
Alfi Nurfazri <alfinurfazri@gmail.com>
2022-05-11 16:42:50 +0700

    menambahkan math rules

--- course-script/opperation.js
+++ course-script/opperation.js
@@ -4,4 +4,18 @@ console.log(10+4);
 console.log(10-4);
 console.log(10*4);
 console.log(10/4);
-console.log(10%4);
+console.log(10%4); //sisa 2
+console.log(10%3); //sisa 1
+
+console.log((10 + 3 - 5 ) * 10);
+
+// let result = (10 + 3 -5) * 10;
+// result = 10 * 4;
+// result++; //result = result + 1;
+// result--; //result = result - 1;
+// result /= 5; //result = result / 5;
+// result *= 5; //result = result * 5;
+// console.log(result);
+
+console.log('Alfi' + ' ' + 'Nurfazri');
+console.log('Alfi' - 'fi');
