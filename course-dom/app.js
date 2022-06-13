8d9448c0ef510b5991fed6dd54b4640ef647c0d2
Alfi Nurfazri <alfinurfazri@gmail.com>
2022-06-13 11:19:45 +0700

    searching the dom for element

---
+++ course-dom/app.js
@@ -0,0 +1,8 @@
+// console.dir(window.document);
+
+//document.body.children[1].children[0].href = 'https://google.com';
+// console.dir(document);
+// alert();
+// window.alert();
+
+document.getElementById('external-link');
