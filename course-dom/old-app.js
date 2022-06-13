a05b3efe459fd6b8e9ba725851a77f130e57527b
Alfi Nurfazri <alfinurfazri@gmail.com>
2022-06-13 13:02:28 +0700

    menambahkan first event listerner click

---
+++ course-dom/old-app.js
@@ -0,0 +1,55 @@
+// console.dir(window.document);
+
+//document.body.children[1].children[0].href = 'https://google.com';
+// console.dir(document);
+// alert();
+// window.alert();
+
+
+//mencari DOM dengan memlilih HTML element dengan id hanya satu element
+//untuk mencari spesifik element
+let achorElement = document.getElementById('external-link');
+achorElement.href = 'https://google.com';
+
+//mencari DOM dengan memilih HTML element yang sesuai pertama dengan menggunakan css selector atau html element
+//untuk mencari sesuai kondisi
+anchorElement = document.querySelector('p a');
+anchorElement.href = 'https://facebook.com';
+
+//add an element
+//1. buat element baru
+
+let newAnchorElement = document.createElement('a');
+newAnchorElement.href = 'https://twitter.com';
+newAnchorElement.textContent = 'This leads to twitter!';
+
+//2. mendapatkan akses ke parent element yang menyimpan element baru
+
+let firstParagraph = document.querySelector('p');
+
+
+//3. memasukan element baru ke dalam parent element content
+
+firstParagraph.append(newAnchorElement);
+
+
+//remove element
+//1. memilih element yang ingin dihapus
+
+let firstH1Element = document.querySelector('h1');
+
+//2. hapus
+
+firstH1Element.remove();
+//firstH1Element.parentElement.removeChild(firstH1Element); //untuk browser lama
+
+//move element
+
+firstParagraph.parentElement.append(firstParagraph);
+
+//innerHTML memberikan akses ke semua content di HTML element
+
+console.log(firstParagraph.innerHTML);
+
+firstParagraph.innerHTML = 'Hi! This is <strong>important!</strong>' +
+    'another value';
