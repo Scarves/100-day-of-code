bb813a7ec23008525a83b4b2f69b560ce2a5bf0f
Alfi Nurfazri <alfinurfazri@gmail.com>
2022-06-13 13:55:02 +0700

    menambahkan user input event

--- course-dom/app.js
+++ course-dom/app.js
@@ -2,6 +2,16 @@ let paragraphElement = document.querySelector('p'); //mendapatkan akses ke p
 
 function changeParagraphText(){
     paragraphElement.textContent = 'Clicked!';
+    console.log('paragraph clicked!');
 }
 
 paragraphElement.addEventListener('click', changeParagraphText);
+
+let inputElement = document.querySelector('input');
+
+function retrieveUserInput() {
+    let enteredText = inputElement.value;
+    console.log(enteredText);
+}
+
+inputElement.addEventListener('input', retrieveUserInput);
