68a47bd33871b7163b9c8cf543ae911f61085315
Alfi Nurfazri <alfinurfazri@gmail.com>
2022-06-18 20:23:18 +0700

    menambahkan if else konsep ke conditional-code.js

--- course-dom/conditional-code.js
+++ course-dom/conditional-code.js
@@ -3,3 +3,17 @@ const myName = 'Max';
 if (myName === 'Max'){
     console.log('Hello');
 }
+
+let isLoggedIn = true
+
+if (!isLoggedIn) {
+    console.log('User is NOT logged in!');
+}
+
+const enteredUserName = ''; //'' = 0 = false
+// '0' = 1 = true
+//'false' = 1 = true
+
+if (enteredUserName) {
+    console.log('Input is valid!');
+}
