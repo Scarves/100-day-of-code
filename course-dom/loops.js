33873abbce6e9183aedbeeb39f09940e3393e2f6
Alfi Nurfazri <alfinurfazri@gmail.com>
2022-06-19 05:18:30 +0700

    menambahkan komen pada loops.js

--- course-dom/loops.js
+++ course-dom/loops.js
@@ -1,9 +1,16 @@
+// loop for
 for (let i = 0; i < 10; i++) {
     console.log(i);
 }
 
 const users = ['Max', 'Anna', 'Joel'];
 
+// loop for of
 for (const user of users){
     console.log(user);
 }
+
+// looping array selain menggunakan for of
+// for (let i = 0; i < someArray.length; i++) {
+//     console.log(someArray[i]);
+// }
