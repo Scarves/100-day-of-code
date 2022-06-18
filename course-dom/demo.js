356ef9f0c3867caa79d2109c618ddd0abc248b42
Alfi Nurfazri <alfinurfazri@gmail.com>
2022-06-18 20:12:17 +0700

    enambahkan if else statment pada demo.js

--- course-dom/demo.js
+++ course-dom/demo.js
@@ -1,15 +1,28 @@
-let productNameInputElement = document.getElementById('product-name');
-let remainingCharsElement = document.getElementById('remaining-chars');
+const productNameInputElement = document.getElementById('product-name');
+const remainingCharsElement = document.getElementById('remaining-chars');
 
 // console.dir(productNameInputElement);
 
-let maxAllowedChars = productNameInputElement.maxLength;
+const maxAllowedChars = productNameInputElement.maxLength;
 
 function updateRemainingCharacters(event){
-    let enteredText = event.target.value;
-    let enteredTextLength = enteredText.length;
+    const enteredText = event.target.value;
+    const enteredTextLength = enteredText.length;
 
-    let remainingCharacters = maxAllowedChars - enteredTextLength;
+    const remainingCharacters = maxAllowedChars - enteredTextLength;
+    
+    if (remainingCharacters === 0 ){
+        remainingCharsElement.classList.add('eror');
+        productNameInputElement.classList.add('eror');
+    } else if (remainingCharacters <= 10 ){
+        remainingCharsElement.classList.add('warning');
+        productNameInputElement.classList.add('warning');
+        remainingCharsElement.classList.remove('eror');
+        productNameInputElement.classList.remove('eror');
+    } else {
+        remainingCharsElement.classList.remove('warning');
+        productNameInputElement.classList.remove('warning');
+    }
 
     remainingCharsElement.textContent = remainingCharacters;
 }
