0e5323ab33476bac7a1e29fc0a87d6823f5bb59b
Alfi Nurfazri <alfinurfazri@gmail.com>
2022-11-20 00:32:28 +0700

    pushing

--- tic-tac-toe/script/game.js
+++ tic-tac-toe/script/game.js
@@ -1,42 +1,143 @@
-function startNewGame() {
-    if (players[0].name === '' || players[1].name === '') {
-        alert('Tolong masukan nama kedua pemain')
-        return;
-    }
+function resetGameStatus() {
+  activePlayer = 0;
+  currentRound = 1;
+  gameIsOver = false;
+  gameOverElement.firstElementChild.innerHTML =
+    'Kamu Menang, <span id="winner-name">NAMA PEMAIN</span>!';
+    gameOverElement.style.display = 'none';
 
-    activePlayerNameElement.textContent = players[activePlayer].name;
-    gameAreaElement.style.display = 'block';
+    let gameBoardIndex = 0;
+    for (let i=0; i<3; i++){
+      for (let j=0; j<3; j++){
+        gameData[i][j] = 0;
+        const gameBoardItemElement = gameBoardElement.children[gameBoardIndex];
+        gameBoardItemElement.textContent = '';
+        gameBoardItemElement.classList.remove('disabled');
+        gameBoardIndex++;
+      }
+    }
+}
+
+function startNewGame() {
+  if (players[0].name === '' || players[1].name === '') {
+    alert('Tolong masukan nama kedua pemain!');
+    return;
+  }
+
+  resetGameStatus();
+
+  activePlayerNameElement.textContent = players[activePlayer].name;
+  gameAreaElement.style.display = "block";
 }
 
 function switchPlayer() {
-    if (activePlayer === 0) {
-        activePlayer = 1;
-    } else {
-        activePlayer = 0;
-    }
-    activePlayerNameElement.textContent = players[activePlayer].name;
+  if (activePlayer === 0) {
+    activePlayer = 1;
+  } else {
+    activePlayer = 0;
+  }
+  activePlayerNameElement.textContent = players[activePlayer].name;
 }
 
-function selectedGameField(event) {
-    if (event.target.tagName !== 'LI') {
-        return;
+function selectGameField(event) {
+  if (event.target.tagName !== 'LI'|| gameIsOver) {
+    return;
+  }
+
+  const selectedField = event.target;
+  const selectedColumn = selectedField.dataset.col - 1;
+  const selectedRow = selectedField.dataset.row - 1;
+
+  if (gameData[selectedRow][selectedColumn] > 0) {
+    alert('Tolong pilih tempat yang kosong!');
+    return;
+  }
+
+  selectedField.textContent = players[activePlayer].symbol; //player[0]
+  selectedField.classList.add('disabled');
+
+  gameData[selectedRow][selectedColumn] = activePlayer + 1;
+
+  const winnerId = checkForGameOver();
+
+  if (winnerId !== 0) {
+    endGame(winnerId);
+  }
+
+  currentRound++;
+  switchPlayer();
+}
+
+function checkForGameOver() {
+  //mengecek kesamaan row
+  for (let i = 0; i < 3; i++) {
+    if (
+      gameData[i][0] > 0 &&
+      gameData[i][0] === gameData[i][1] &&
+      gameData[i][1] === gameData[i][2]
+    ) {
+      return gameData[i][0];
     }
-    
-    const selectedField = event.target;
-    const selectedColumn = selectedField.dataset.col - 1;
-    const selectedRow = selectedField.dataset.row -1;
+  }
 
-    if (gameData[selectedRow][selectedColumn] > 0) {
-        alert ('Tolong pilih tempat yang kosong!');
-        return;
+  //   if (
+  //     gameData[1][0] > 0 &&
+  //     gameData[1][0] === gameData[1][1] &&
+  //     gameData[1][1] === gameData[1][2]
+  //   ) {
+  //     return gameData[1][0];
+  //   }
+
+  //   if (
+  //     gameData[2][0] > 0 &&
+  //     gameData[2][0] === gameData[2][1] &&
+  //     gameData[2][1] === gameData[2][2]
+  //   ) {
+  //     return gameData[2][0];
+  //   }
+
+  //mengecek kesamaan kolom
+  for (let i = 0; i < 3; i++) {
+    if (
+      gameData[0][i] > 0 &&
+      gameData[0][i] === gameData[1][i] &&
+      gameData[0][i] === gameData[2][i]
+    ) {
+      return gameData[0][i];
     }
-    
-    selectedField.textContent = players[activePlayer].symbol; //player[0]
-    selectedField.classList.add('disabled');
+  }
+  //mengecek kesamaan diagonal kiri bawah ke atas kanan
+  if (
+    gameData[0][0] > 0 &&
+    gameData[0][0] === gameData[1][1] &&
+    gameData[1][1] === gameData[2][2]
+  ) {
+    return gameData[0][0];
+  }
 
+  if (
+    gameData[2][0] > 0 &&
+    gameData[2][0] === gameData[1][1] &&
+    gameData[1][1] === gameData[0][2]
+  ) {
+    return gameData[2][0];
+  }
 
-    gameData[selectedRow][selectedColumn] = activePlayer + 1;
-    
+  if (currentRound === 9) {
+    return -1;
+  }
+  return 0;
+}
 
-    switchPlayer();
+function endGame(winnerId) {
+  gameIsOver = true;
+  gameOverElement.style.display = "block";
+
+  if (winnerId > 0) {
+    const winnerName = players[winnerId - 1].name;
+    gameOverElement.firstElementChild.firstElementChild.textContent =
+      winnerName;
+  } else {
+    gameOverElement.firstElementChild.textContent = "it's a draw!";
+  }
 }
